c28a65813cc72abd3e0b13e9dc6d4f64b9717bb7
Olenderpj <Olenderpj1@gmail.com>
2021-12-15 13:13:37 -0700

    Wired a basic view to the project application

--- project/views.py
+++ project/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Hello, Welcome to the index page')
+
+
+def individual_post(request):
+    return HttpResponse('Hi, this is where an individual post will be.')
